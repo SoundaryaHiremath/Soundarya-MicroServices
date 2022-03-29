6b72e351e6f8dcd63e73f44c15262fa61f7d343c
SoundaryaHiremath <soundaryahiremath28@gmail.com>
2022-03-29 17:59:56 +0530

    Updating Files

---
+++ MICROSERVICES/UserService/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source = US-3WGFZH3; initial catalog=UserMicroservice; integrated security=true"); 
+        }
+    } 
+}
